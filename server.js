b721a8921ebc29877bd3c2a4eefbe6045dd018ec
David White <dwhite0627@gmail.com>
2020-05-08 20:27:17 -0400

    Add index.js and server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const server = express();
+
+// Middleware
+const cors = require("cors");
+const helmet = require("helmet");
+const authRoute = require("./routes/auth/auth-router");
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// Routes
+server.use("/api", authRoute);
+
+// Error middleware
+server.use((err, req, res, next) => {
+  console.log(err);
+  if (err) {
+    return res.status(500).json({
+      message: "There was an error performing the required operation",
+      validation: [],
+      data: {},
+    });
+  }
+});
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "API up", validation: [], data: {} });
+});
+
+module.exports = server;
