932a6379ed6b5d5dc56ff3d3713e07e0c51fcce6
David White <dwhite0627@gmail.com>
2020-05-11 11:46:15 -0400

    Add knexfile, db config and migrations for the users table

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true, 
+    connection: {
+      filename: './data/user.db3',
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true, 
+    connection: {
+      filename: './data/test.db3',
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+  },
+};
