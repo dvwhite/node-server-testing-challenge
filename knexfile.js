a44db270d9efc3775d990fca49f193484da68052
David White <dwhite0627@gmail.com>
2020-05-13 21:10:11 -0400

    Add test database support using cross-env in the db config file, modify package.json scripts

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true, 
     connection: {
-      filename: './data/test.db3',
+      filename: './data/users-test.db3',
     },
     migrations: {
       directory: './data/migrations'
