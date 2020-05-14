4545f7067cb58e35dd8a79306daf145258287a37
David White <dwhite0627@gmail.com>
2020-05-14 07:38:19 -0400

    Test the server for 404 and 500 error codes

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -5,6 +5,13 @@ const db = require("./../data/dbConfig");
 // Middleware imports
 const errorHandler = require("../middleware/error-middleware");
 
+const errorObj = {
+  Test: {
+    name: 'Trip the error middleware',
+    desc: 'Test of the error middleware, seeing this is intentional'
+  }
+};
+
 describe("GET /", () => {
   // Mocked req, res Express objects
   let req, res;
@@ -14,26 +21,25 @@ describe("GET /", () => {
   beforeEach(() => {
     req = {
       params: {},
-      body: {}
-    }
+      body: {},
+    };
 
     res = {
       data: null,
       code: null,
-      status (status) {
-        this.code = status
-        return this
+      status(status) {
+        this.code = status;
+        return this;
       },
-      send (payload) {
-        this.data = payload
+      send(payload) {
+        this.data = payload;
       },
-      json (payload) {
-        this.data = JSON.stringify(payload)
-        return this
-      }
-    }
+      json(payload) {
+        this.data = JSON.stringify(payload);
+      },
+    };
 
-    next.mockClear()
+    next.mockClear();
   });
 
   afterAll(async () => {
@@ -41,23 +47,22 @@ describe("GET /", () => {
   });
 
   it("returns a 200 status code", () => {
-    return request(server).get("/")
+    return request(server)
+      .get("/")
       .expect(200)
       .expect("Content-Type", /json/)
-      .then(res => {
-        expect(res.body.message).toBe("API up")
-      })
+      .then((res) => {
+        expect(res.body.message).toBe("API up");
+      });
   });
 
   it("returns a 404 status code for routes that don't exist", () => {
-    return request(server).get("/doesntexist")
-      .expect(404)
+    return request(server).get("/doesntexist").expect(404);
   });
 
-  it("returns a 500 status code when there's an error", async done => {
-    await errorHandler(new Error(), req, res, next)
+  it("returns a 500 status code when there's an error", async (done) => {
+    errorHandler(errorObj, req, res, next);
     expect(500).toBe(500);
     done();
   });
-
 });
