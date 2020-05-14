a05604c7bcd1e04cbe7eb79745bdbbeb4b1424c2
David White <dwhite0627@gmail.com>
2020-05-13 22:19:02 -0400

    Add conditional server listening to prevent issues with supertest

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const server = require("./server");
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-});
+if (!module.parent) {
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  });
+};
