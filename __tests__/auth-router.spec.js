f47367646e70ca101e08e3e438b1f0fc448b4888
David White <dwhite0627@gmail.com>
2020-05-15 08:58:29 -0400

    Add test for /register route to verify it doesn't insert a user multiple times

--- __tests__/auth-router.spec.js
+++ __tests__/auth-router.spec.js
@@ -9,18 +9,18 @@ const dbHasTruncated = async () => {
   // Verifies that the database has truncated
   try {
     const users = await find();
-    return (users && users.length ? false : true);
+    return users && users.length ? false : true;
   } catch (err) {
     console.log(err);
     return false;
   }
-}
+};
 
 const verifyProperties = (obj, props) => {
   // Verifies that the object has the required properties
   objKeys = Object.keys(obj);
-  return props.every(key => objKeys.includes(key));
-}
+  return props.every((key) => objKeys.includes(key));
+};
 
 // Db helpers
 const { find } = require("../routes/users/users-model");
@@ -32,11 +32,23 @@ const hash = bcrypt.hashSync(defaultPW, Number(process.env.HASHES));
 const testUser = {
   username: "test User",
   password: hash,
-  role: 1
+  role: 1,
 };
 
+const authError = {
+  message: "Invalid Credentials",
+  validation: [],
+  data: {},
+};
+
+const serverError = {
+  message: "There was a problem completing the required operation",
+  validation: [],
+  data: {},
+}
+
 describe("/register", () => {
-  beforeEach(async done => {    
+  beforeEach(async (done) => {
     try {
       await db("users").truncate();
       done();
@@ -46,12 +58,12 @@ describe("/register", () => {
     }
   });
 
-  it("inserts a new user into the db with /register", async () => {
+  it("inserts a new user into the db", async () => {
     // Ensure users have been truncated properly
     const truncated = await dbHasTruncated();
     expect(truncated).toBe(true);
     // Test the endpoint
-    const res = await request(server).post("/api/register").send(testUser)
+    const res = await request(server).post("/api/register").send(testUser);
     expect(res.statusCode).toBe(201);
     expect(res.type).toBe("application/json");
     expect(verifyProperties(res.body.data, ["username", "role"])).toBe(true);
@@ -59,14 +71,34 @@ describe("/register", () => {
     expect(res.body.data.role).toBe(testUser.role);
   });
 
-  afterAll(async done => {
+  it("doesn't insert an existing user into the database", async done => {
+    // Ensure users have been truncated properly
+    let truncated = await dbHasTruncated();
+    expect(truncated).toBe(true);
+    // Test the endpoint
+    const res = await request(server).post("/api/register").send(testUser);
+    truncated = await dbHasTruncated();
+    expect(truncated).toBe(false);
     try {
-      await db.destroy();
-      done()
+      const res = await request(server).post("/api/register").send(testUser);
+      expect(res.statusCode).toBe(500);
+      expect(res.type).toBe("application/json");
+      expect(JSON.parse(res.text)).toEqual(serverError);
+      const users = await find();
+      expect(users).toHaveLength(1);
+      done();
     } catch (err) {
-      console.log("Unable to close the database connection", err)
       done(err)
     }
   });
 
+  afterAll(async (done) => {
+    try {
+      await db.destroy();
+      done();
+    } catch (err) {
+      console.log("Unable to close the database connection", err);
+      done(err);
+    }
+  });
 });
